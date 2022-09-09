e8420f221fc06a956fe737aab31e69e3da4bc281
Peter Pham <peterpham.pct000@gmail.com>
2022-09-09 14:33:32 +0700

    Add new order_drink method in menu.py

--- Day 16 - Intermediate - Object Oriented Programming/menu.py
+++ Day 16 - Intermediate - Object Oriented Programming/menu.py
@@ -1,5 +1,9 @@
+import numpy as np
+
+
 class MenuItem:
     """Models each Menu Item."""
+
     def __init__(self, name, water, milk, coffee, cost):
         self.name = name
         self.cost = cost
@@ -12,6 +16,7 @@ class MenuItem:
 
 class Menu:
     """Models the Menu with drinks."""
+
     def __init__(self):
         self.menu = [
             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
@@ -32,3 +37,23 @@ class Menu:
             if item.name == order_name:
                 return item
         print("Sorry that item is not available.")
+
+    def order_drink(self, *args):
+        """Order as many drinks as you want. Returns items if they exist, otherwise returns None"""
+        item_dict = {item.name: 0 for item in self.menu}
+        wrong_order = []
+        for arg in args:
+            if type(arg) == list:
+                for item in arg:
+                    if item in item_dict.keys():
+                        item_dict[item] = item_dict[item] + 1
+                    else:
+                        wrong_order.append(item)
+                        print(f'Sorry we don\'t have {item}')
+            else:
+                if arg in item_dict.keys():
+                    item_dict[arg] = item_dict[arg] + 1
+                else:
+                    wrong_order.append(arg)
+                    print(f'Sorry we don\'t have {arg}')
+        return item_dict
